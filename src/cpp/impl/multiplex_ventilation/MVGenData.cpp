34190eb8c1661dc9367fcec373e9885490b309be
KHQ\jeff.webb <jeff.webb@kitware.com>
2020-04-06 23:50:21 -0400

    Updated the patient loop. There are still a couple of to-dos and it needs to be tested.

--- src/cpp/impl/multiplex_ventilation/MVGenData.cpp
+++ src/cpp/impl/multiplex_ventilation/MVGenData.cpp
@@ -40,26 +40,24 @@ std::string to_scientific_notation(float f)
 //--------------------------------------------------------------------------------------------------
 bool GenerateStabilizedPatients()
 {
+  // Loop parameters
   float minCompliance_L_Per_cmH2O = 0.010f;
   float maxCompliance_L_Per_cmH2O = 0.050f;
   float stepCompliance_L_Per_cmH2O = 0.005f;
-  float minImpairment = 3.0f;
-  float maxImpairment = 9.0f;
-  float stepImpairment = 0.3f;
   int minPEEP_cmH2O = 10;
   int maxPEEP_cmH2O = 20;
   int stepPEEP_cmH2O = 2;
-  int resistance_cmH2O_s_Per_L = 5;
+  float minImpairment = 3.0f;
+  float maxImpairment = 9.0f;
+  float stepImpairment = 0.3f;
 
-  //TODO: These will be determined through a two-step stabilization process ----
-  //Instead of PIP, we should probably use driving pressure (PIP-PEEP)
-  int minPIP_cmH2O = 20;
-  int maxPIP_cmH2O = 40;
-  int stepPIP_cmH2O = 2;
-  float minFiO2 = 0.10f;
-  float maxFiO2 = 0.95f;
-  float stepFiO2 = 0.5f;
-  //----------------------------------------------------------------------------
+  // Constants
+  int resistance_cmH2O_s_Per_L = 5;
+  int respirationRate_Per_Min = 20;
+  float IERatio = 0.5f;
+
+  // Initial Settings
+  float ambientFiO2 = 0.21f;
 
   Logger logger("MultiplexVentilationGenData.log");
   SESubstanceManager subMgr(&logger);
@@ -84,12 +82,12 @@ bool GenerateStabilizedPatients()
   mv.SetConnection(eMechanicalVentilator_Connection::Tube);
   mv.SetControl(eMechanicalVentilator_Control::PC_CMV);
   mv.SetDriverWaveform(eMechanicalVentilator_DriverWaveform::Square);
-  mv.GetRespiratoryRate().SetValue(20, FrequencyUnit::Per_min);
-  mv.GetInspiratoryExpiratoryRatio().SetValue(0.5);
-  mv.GetPeakInspiratoryPressure().SetValue(28, PressureUnit::cmH2O);
-  mv.GetPositiveEndExpiredPressure().SetValue(10, PressureUnit::cmH2O);
+  mv.GetRespiratoryRate().SetValue(respirationRate_Per_Min, FrequencyUnit::Per_min);
+  mv.GetInspiratoryExpiratoryRatio().SetValue(IERatio);
+  mv.GetPeakInspiratoryPressure().SetValue(minPEEP_cmH2O, PressureUnit::cmH2O);
+  mv.GetPositiveEndExpiredPressure().SetValue(minPEEP_cmH2O, PressureUnit::cmH2O);
   SESubstanceFraction* FiO2 = &mv.GetFractionInspiredGas(*Oxygen);
-  FiO2->GetFractionAmount().SetValue(0.3);
+  FiO2->GetFractionAmount().SetValue(ambientFiO2);
 
   std::string baseName;
   pulse::multiplex_ventilator::bind::PatientStateListData patients;
@@ -102,43 +100,75 @@ bool GenerateStabilizedPatients()
     std::cout << "[" << level << "] " << filename << "::" << line << " " << message;
   });
 
-  for (int c=minCompliance_L_Per_cmH2O; c<=maxCompliance_L_Per_cmH2O; c+=stepCompliance_L_Per_cmH2O)
+  double breathPeriod_s = 60.0 / respirationRate_Per_Min;
+
+  for (float c = minCompliance_L_Per_cmH2O; c <= maxCompliance_L_Per_cmH2O; c += stepCompliance_L_Per_cmH2O)
   {
     overrides.AddScalarProperty("RespiratoryCompliance", c, VolumePerPressureUnit::L_Per_cmH2O);
 
-    for (float i=minImpairment; i<=maxImpairment; i+=stepImpairment)
+    double currentPIP_cmH2O = 0;
+    for (int peep = minPEEP_cmH2O; peep <= maxPEEP_cmH2O; peep += stepPEEP_cmH2O)
     {
-      impairedAlveolarExchange.GetSeverity().SetValue(i);
-      pulmonaryShunt.GetSeverity().SetValue(i);
+      mv.GetInspiratoryExpiratoryRatio().SetValue(peep);
 
-      for (int peep=minPEEP_cmH2O; peep<=maxPEEP_cmH2O; peep+=stepPEEP_cmH2O)
+      // Step the PIP until we get a TidalVolume between 6 ml/kg (ideal body weight)
+      double targetTidalVolume_mL = 6.0 * 77.0; //Aaron, how do I get the patient's ideal body weight here?
+      double currentTidalVolume_mL = 0.0;
+      currentPIP_cmH2O = peep;
+      while (currentTidalVolume_mL < targetTidalVolume_mL)
       {
-        mv.GetInspiratoryExpiratoryRatio().SetValue(peep);
+        currentPIP_cmH2O = peep + 1.0;
+        // Aaron - simulation for a breathPeriod_s
+        // currentTidalVolume_mL = get TV
+      }
+
+      // This PIP is now good for all patients with this compliance and PEEP combination
+      mv.GetPeakInspiratoryPressure().SetValue(currentPIP_cmH2O, PressureUnit::cmH2O);
+
+      double currentFiO2 = ambientFiO2;
+      for (float i = minImpairment; i <= maxImpairment; i += stepImpairment)
+      {
+        impairedAlveolarExchange.GetSeverity().SetValue(i);
+        pulmonaryShunt.GetSeverity().SetValue(i);
 
         // Construct our engine name
-        baseName = "comp="+std::to_string(c)+"_imp="+to_scientific_notation(i)+"_peep="+std::to_string(peep);
+        baseName = "comp=" + std::to_string(c) + "_imp=" + to_scientific_notation(i) + "_peep=" + std::to_string(peep);
         logger.Info("Creating engine " + baseName);
-        auto pulse = CreatePulseEngine("./states/multiplex_ventilation/"+baseName+".log");
+        auto pulse = CreatePulseEngine("./states/multiplex_ventilation/" + baseName + ".log");
         pulse->SerializeFromFile("./states/StandardMale@0s.json", SerializationFormat::JSON);
 
-        // Step the PIP until we get a TidalVolume between 6-8 mL
-        for (int pip=minPIP_cmH2O; pip<maxPIP_cmH2O; pip+=stepPIP_cmH2O)
-        {
-          mv.GetPeakInspiratoryPressure().SetValue(pip, PressureUnit::cmH2O);
-        }
-
         // Step the FiO2 until we get an SpO2 of > 90
-        for (float o2=minFiO2; o2<maxFiO2; o2+=stepFiO2)
+        double SpO2 = 0.1;
+        while (SpO2 < 0.9)
         {
-          FiO2->GetFractionAmount().SetValue(o2);
+          if (currentFiO2 > 0.995)
+          {
+            currentFiO2 = 0.995;
+            break;
+          }
+
+          //Start with the prvious FiO2 because this should require a higher value to acheive our desired SpO2
+          FiO2->GetFractionAmount().SetValue(currentFiO2);
+
+          // Run until stable
+          double previousSPO2 = 0.0;
+          double criteria = 0.01; //TODO: We need to play with this
+          while (abs(SpO2 - previousSPO2) > criteria)
+          {
+            previousSPO2 = SpO2;
+            //Aaron - simulation for a breathPeriod_s
+            //SpO2 = get spO2
+          }
+
+          currentFiO2 += 0.01;
         }
 
         // Save our state
-        logger.Info("Saving engine state"+baseName+".json");
-        pulse->SerializeToFile("./states/multiplex_ventilation/"+baseName+".json",SerializationFormat::JSON);
+        logger.Info("Saving engine state" + baseName + ".json");
+        pulse->SerializeToFile("./states/multiplex_ventilation/" + baseName + ".json", SerializationFormat::JSON);
         // Append to our "list" of generated states
         auto patientData = patients.add_patients();
-        patientData->set_statefile("./states/multiplex_ventilation/"+baseName+".json");
+        patientData->set_statefile("./states/multiplex_ventilation/" + baseName + ".json");
         patientData->set_compliance_l_per_cmh2o(c);
         patientData->set_impairmentfraction(i);
         patientData->set_peep_cmh2o(peep);
