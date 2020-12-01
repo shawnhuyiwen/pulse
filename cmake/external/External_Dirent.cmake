722b38a5220853d89c67967b85bd697326d9cc20
Aaron Bray <aaron.bray@kitware.com>
2020-12-01 12:49:05 -0500

    CMake updates

--- cmake/external/External_Dirent.cmake
+++ cmake/external/External_Dirent.cmake
@@ -7,6 +7,7 @@ add_external_project_ex( Dirent
   URL "https://github.com/tronkko/dirent/archive/1.23.2.zip"
   URL_HASH MD5=43514791ab73ef5ac7c490afc7c3bab2
   CMAKE_CACHE_ARGS
+    -DBUILD_TESTING:BOOL=OFF
     -DDIRENT_BUILD_TESTS:BOOL=OFF
   RELATIVE_INCLUDE_PATH ""
   DEPENDENCIES ""
