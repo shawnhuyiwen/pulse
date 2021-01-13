10f991fe3404270a5c2b4a8a2b2889333c73d7c6
Harald Scheirich <harald.scheirich@kitware.com>
2021-01-13 16:20:22 -0500

    Update protobuf and Eigen to latest

--- cmake/external/External_protobuf.cmake
+++ cmake/external/External_protobuf.cmake
@@ -14,8 +14,8 @@ endif()
 include(AddExternalProject)
 define_external_dirs_ex(protobuf)
 add_external_project_ex( protobuf
-  URL https://github.com/protocolbuffers/protobuf/releases/download/v3.12.2/protobuf-all-3.12.2.zip
-  URL_MD5 ec63c1566640a5873566e76ec8eb1398
+  URL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protobuf-all-3.14.0.zip
+  URL_MD5 82cba836a6085599fae030d6c071d4a3
   SOURCE_SUBDIR ./cmake
   CMAKE_CACHE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=OFF
