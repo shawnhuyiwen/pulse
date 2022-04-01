7d326ca4341f095a9c03241c472130ce5029349a
Emily Veenhuis <emily.veenhuis@kitware.com>
2022-04-01 11:25:30 -0500

    file structure and cmake updates

--- src/cpp/study/patient_variability/main.cpp
+++ src/cpp/study/patient_variability/main.cpp
@@ -2,16 +2,39 @@
    See accompanying NOTICE file for details.*/
 
 #include "PVRunner.h"
+#include "PVGenerator.h"
+
+#include "cdm/utils/FileUtils.h"
 
 using namespace pulse::study::patient_variability;
 
 int main(int argc, char* argv[])
 {
- 
+  std::string mode = ""; // Default is to run the manual else below
+  if (argc <= 1)
+  {
+    // Adjust comments to run the mode you want
+    //mode = "clear";
+    mode = "genPatients";
+  }
+  else
+  {
+    mode = argv[1];
+  }
+  
+  if(mode == "genPatients")
+  {
+    PVGenerator pvg("./test_results/patient_variability/PatientVariabilityGen.log");
+    pulse::study::bind::patient_variability::SimulationListData simList;
+    simList.set_outputrootdir("./test_results/patient_variability");
 
-  PVRunner pvr("./test_results/patient_variability/PatientVariabilityRunner.log");
-  pulse::study::bind::patient_variability::SimulationListData simList;
-  simList.set_outputrootdir("./test_results/patient_variability");
+    return !pvg.Run("./test_results/patient_variability", "./test_results/patient_variability/states_list.json");
+  }
+  else if(mode == "clear")
+  {
+    DeleteDirectory("./test_results/patient_variability");
+  }
+
 
   return 0;
 }
