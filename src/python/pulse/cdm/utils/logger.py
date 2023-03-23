c768477e21017c6f4c2b328599e3f2675d857ea4
Emily Veenhuis <emily.veenhuis@kitware.com>
2023-03-23 16:24:46 -0500

    Python space delimited camel case for pretty print

--- src/python/pulse/cdm/utils/logger.py
+++ src/python/pulse/cdm/utils/logger.py
@@ -1,13 +1,42 @@
 # Distributed under the Apache License, Version 2.0.
 # See accompanying NOTICE file for details.
 from enum import Enum
+from typing import Set
+import re
 
 from pulse.cdm.engine import SEAction
+
+def break_camel_case(string: str, preserve_units: Set[str]):
+    # https://stackoverflow.com/a/9283563
+    camel_case_regex = r"""
+        (            # start the group
+            # alternative 1
+        (?<=[a-z])  # current position is preceded by a lower char
+                    # (positive lookbehind: does not consume any char)
+        [A-Z]       # an upper char
+                    #
+        |   # or
+            # alternative 2
+        (?<!\A)     # current position is not at the beginning of the string
+                    # (negative lookbehind: does not consume any char)
+        [A-Z]       # an upper char
+        (?=[a-z])   # matches if next char is a lower char
+                    # lookahead assertion: does not consume any char
+        )           # end the group"""
+
+    string = re.sub(camel_case_regex, r' \1', string, flags=re.VERBOSE)
+
+    # Return specified units to their original form
+    for unit in preserve_units:
+        spaced_unit = re.sub(camel_case_regex, r' \1', unit, flags=re.VERBOSE)
+        if spaced_unit != unit:
+            string = string.replace(spaced_unit, unit)
+    return string
 
 class ePrettyPrintType(Enum):
     Action = 0
     Condition = 1
-def pretty_print(string: str, print_type: ePrettyPrintType):
+def pretty_print(string: str, print_type: ePrettyPrintType, preserve_camel_case: bool = False):
     typeTag = ""
     if print_type == ePrettyPrintType.Action:
         typeTag = "Action"
@@ -24,6 +53,7 @@ def pretty_print(string: str, print_type: ePrettyPrintType):
 
     lines = [s.rstrip() for s in string.splitlines() if s]
 
+    units = set()
     idx = 0
     while idx < len(lines):
         line = lines[idx].rstrip()
@@ -49,7 +79,9 @@ def pretty_print(string: str, print_type: ePrettyPrintType):
                 if (idx+3) < len(lines) and "Unit:" in lines[idx+3]:
                     idx += 3
                     peek = lines[idx]
-                    line = ''.join([line, peek[(peek.find("Unit:")+5):].rstrip()])
+                    unit = peek[(peek.find("Unit:")+5):].rstrip()
+                    units.add(unit)
+                    line = ''.join([line, unit])
                 else:
                     idx += 2
 
@@ -57,5 +89,7 @@ def pretty_print(string: str, print_type: ePrettyPrintType):
         idx += 1
 
     ret = ret.replace('::', ':')
+    if not preserve_camel_case:
+        ret = break_camel_case(ret, units)
 
     return ret
