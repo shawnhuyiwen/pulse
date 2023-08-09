092a2a6965ee276d648679624cbef3689611d0f4
Emily Veenhuis <emily.veenhuis@kitware.com>
2023-08-09 15:34:31 -0500

    Fix python multiplex ventilation

--- src/python/pulse/study/multiplex_ventilation/engine.py
+++ src/python/pulse/study/multiplex_ventilation/engine.py
@@ -17,14 +17,14 @@ class MVEngine:
     def create_engine(self, simulation: SimulationData):
         # Process requests and setup our results structure
         out = json_format.MessageToJson(simulation)
-        return self.__pulse.create_engine(out, PyPulse.serialization_format.json)
+        return self.__pulse.create_engine(out)
 
     def advance_time_s(self, duration_s: float, sim: SimulationData):
         num_steps = int(duration_s / 0.02)
         for n in range(num_steps):
             if not self.__pulse.advance_time(0.02):
                 return False
-        json = self.__pulse.get_patient_state(PyPulse.serialization_format.json)
+        json = self.__pulse.get_patient_state()
         json_format.Parse(json, sim)
         return True
 
@@ -35,7 +35,7 @@ class MVEngine:
     def process_actions(self, actions: []):
         json = serialize_actions_to_string(actions,eSerializationFormat.JSON)
         #print(json)
-        self.__pulse.process_actions(json,PyPulse.serialization_format.json)
+        self.__pulse.process_actions(json)
 
     def destroy(self):
         self.__pulse.destroy_engine()
